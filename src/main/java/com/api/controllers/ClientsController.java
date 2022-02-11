7eed94cf31b1c37e6705f0c122359ac1964e59b6
Astrik Torres <astriktorres@gmail.com>
2022-02-10 21:51:26 -0600

    añade cross origin

--- src/main/java/com/api/controllers/ClientsController.java
+++ src/main/java/com/api/controllers/ClientsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
+// le da permisos a mi front
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/clients/")
 public class ClientsController {
 	
